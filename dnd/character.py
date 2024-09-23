d618971f2d73230609e47a1c7211ceb5fde84f89
erik-larkin <erlarkin@tcd.ie>
2024-09-23 22:56:39 +0100

    add alignments

--- dnd/character.py
+++ dnd/character.py
@@ -1,10 +1,34 @@
 class Ability:
-	def __init__(self, score: int) -> None:
-		self.score = score
-	
-	@property
-	def modifier(self) -> int:
-		return (self.score - 10) // 2
-	
-	def __str__(self) -> str:
-		return f"{self.score} ({self.modifier:+})"
+    """Class representing ability scores and modifiers, such as Strength,
+    Charisma, Dexterity, etc."""
+
+    def __init__(self, score: int) -> None:
+        self.score = score
+    
+    @property
+    def modifier(self) -> int:
+        return (self.score - 10) // 2
+    
+    def __str__(self) -> str:
+        return f"{self.score} ({self.modifier:+})"
+
+_ORDERS = set(["neutral", "lawful", "chaotic"])
+_MORALITIES = set(["neutral", "good", "evil"])
+
+class Alignment:
+    """Class representing character alignments such as Lawful Good, Chaotic
+    Neutral, etc."""
+
+    def __init__(self, order: str, morality: str) -> None:
+        if order.lower() not in _ORDERS:
+            raise ValueError(f"order must be one of {_ORDERS}")
+        if morality.lower() not in morality:
+            raise ValueError(f"morality must be one of {_MORALITIES}")
+        
+        self.morality = morality.lower()
+        self.order = order.lower()
+    
+    def __str__(self) -> str:
+        if self.morality == "neutral" and self.order == "neutral":
+            return "Neutral"
+        return f"{self.order.title()} {self.morality.title()}"
--- test/character_test.py
+++ test/character_test.py
@@ -1,26 +1,41 @@
-import sys  
+import sys
 sys.path.append("./")
 from dnd.character import *
 
-def test_init() -> None:
-	sut = Ability(10)
-	assert sut.score == 10
-	assert sut.modifier == 0
+class AbilityTest:
+	def test_init() -> None:
+		sut = Ability(10)
+		assert sut.score == 10
+		assert sut.modifier == 0
 
-	sut = Ability(15)
-	assert sut.score == 15
-	assert sut.modifier == 2
+		sut = Ability(15)
+		assert sut.score == 15
+		assert sut.modifier == 2
 
-	sut = Ability(7)
-	assert sut.score == 7
-	assert sut.modifier == -2
+		sut = Ability(7)
+		assert sut.score == 7
+		assert sut.modifier == -2
 
-def test_str() -> None:
-	sut = Ability(10)
-	assert str(sut) == "10 (+0)"
+	def test_str() -> None:
+		sut = Ability(10)
+		assert str(sut) == "10 (+0)"
 
-	sut = Ability(13)
-	assert str(sut) == "13 (+1)"
+		sut = Ability(13)
+		assert str(sut) == "13 (+1)"
 
-	sut = Ability(5)
-	assert str(sut) == "5 (-3)"
+		sut = Ability(5)
+		assert str(sut) == "5 (-3)"
+
+class AlignmentTest:
+	def test_alignment_init() -> None:
+		sut = Alignment("lawful", "good")
+		assert sut.order == "lawful"
+		assert sut.morality == "good"
+
+		sut = Alignment("CHAOTIC", "EVIL")
+		assert sut.order == "chaotic"
+		assert sut.morality == "evil"
+
+		sut = Alignment("neUTral", "nEutral")
+		assert sut.order == "neutral"
+		assert sut.morality == "neutral"
