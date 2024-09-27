ad8699d8ec7280d819c2914949244eac417c09fa
erik-larkin <erlarkin@tcd.ie>
2024-09-27 22:29:44 +0100

    implemented currency

--- dnd/coins.py
+++ dnd/coins.py
@@ -1,31 +1,51 @@
 from __future__ import annotations
 
+
+CP_VALUE_IN_GP = 0.01
+
+PP_VALUE_IN_CP = 1000
+GP_VALUE_IN_CP = 100
+EP_VALUE_IN_CP = 50
+SP_VALUE_IN_CP = 10
+CP_VALUE_IN_CP = 1
+
 class Coins:
-    def __init__(self) -> None:
-        self.copper = 0
-        self.silver = 0
-        self.electrum = 0
-        self.gold = 0
-        self.platinum = 0
+    def __init__(self, cp = 0, sp = 0, ep = 0, gp = 0, pp = 0) -> None:
+        self.cp = self.convert_to_cp_(cp, sp, ep, gp, pp)
     
-    @property
-    def gold_value(self) -> float:
-        return ((self.platinum * 10) + self.gold + (self.electrum / 2) +
-                (self.silver / 10) + (self.copper / 100))
+    def add(self, cp = 0, sp = 0, ep = 0, gp = 0, pp = 0) -> None:
+        self.cp += self.convert_to_cp_(cp, sp, ep, gp, pp)
 
-    def add(self, money: Coins) -> None:
-        self.copper += money.copper
-        self.silver += money.silver
-        self.electrum += money.electrum
-        self.gold += money.gold
-        self.platinum += money.platinum
+    def remove(self, cp = 0, sp = 0, ep = 0, gp = 0, pp = 0) -> bool:
+        owed = self.convert_to_cp_(cp, sp, ep, gp, pp)
+        if self.cp < owed:
+            return False
+        self.cp -= owed
+        return True
 
-    def remove(self, money: Coins) -> None:
-        pass
+    def convert_to_cp_(cp, sp, ep, gp, pp) -> int:
+        return (pp * PP_VALUE_IN_CP + gp * GP_VALUE_IN_CP + ep * EP_VALUE_IN_CP + 
+                sp * SP_VALUE_IN_CP + cp * CP_VALUE_IN_CP)
+    
+    def split(self) -> tuple[int, int, int, int, int]:
+        cp_left = self.cp
+
+        def take(cp_left, coin_value):
+            if cp_left > 0:
+                coins = cp_left // coin_value
+                return (coins, cp_left - (coins * coin_value)) 
+
+        pp, cp_left = take(cp_left, PP_VALUE_IN_CP)
+        gp, cp_left = take(cp_left, GP_VALUE_IN_CP)
+        ep, cp_left = take(cp_left, EP_VALUE_IN_CP)
+        sp, cp_left = take(cp_left, SP_VALUE_IN_CP)
+        cp, cp_left = take(cp_left, CP_VALUE_IN_CP)
+
+        return pp, gp, ep, sp, cp
 
     def __str__(self) -> str:
+        pp, gp, ep, sp, cp = self.split()
         return (
-            f"Total GP value: {self.gold_value}\n"
-            f"PP: {self.platinum}; GP: {self.gold}; EP: {self.electrum}; "
-            f"SP: {self.silver}; CP: {self.copper}"
+            f"{pp} PP, {gp} GP, {ep} EP, {sp} SP, {cp} CP\n"
+            f"Total GP value: {self.cp * CP_VALUE_IN_GP}"
         )
